fae67ee646617a445959857eb638b3420f1442d5
root <root@localhost.localdomain>
2017-08-07 00:22:33 +0900

    BAEKJOON

---
+++ stage_1/1000_A+B.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner scr = new Scanner(System.in);
+		System.out.println(scr.nextInt()+scr.nextInt());
+	}
+}
