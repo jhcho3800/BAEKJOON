3dde5fb9461b5cb03c34a8783d9279d3ef38da97
root <root@localhost.localdomain>
2017-08-09 00:28:15 +0900

    stage 1

---
+++ stage_1/10718_Welovekrill.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void Main(String[] args){
+       System.out.println("강한친구 대한육군");
+       System.out.println("강한친구 대한육군");
+    }
+}
---
+++ stage_1/7287_register.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void Main(String[] args){
+        System.out.println("3");
+        System.out.println("hoony");
+    }
+}
