54ebd92174af2bade7472ab811f1ad36eba84a4b
root <root@localhost.localdomain>
2017-08-20 00:52:35 +0900

    A-B

---
+++ stage_2/1001_A-B.java
@@ -0,0 +1,15 @@
+/*
+ * 백준 온라인 저지 1001번(A-B) 문제풀이
+ * 
+ * @see https://www.acmicpc.net/problem/1001
+ * @Created by Jeonghun on 2017-08-20
+ **/
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		int a = System.in.read()-48;
+		int b = System.in.read();
+		int c = System.in.read()-48;	
+		System.out.println(a-c);
+	}
+}
