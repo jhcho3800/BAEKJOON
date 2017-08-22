e580a4e24e3daff39e554016e9dffeba4f96e123
root <root@localhost.localdomain>
2017-08-22 17:21:03 +0900

    1008_A/B

---
+++ stage_2/1008.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/*
+ * 백준 온라인 저지 1008번(A/B) 문제풀이
+ *
+ * @see https://www.acmicpc.net/problem/1008
+ * @Created by Jeonghun on 2017-08-22
+ **/
+
+public class Main{
+	public static void main(String[] args) {
+		Scanner scr = new Scanner(System.in);
+		
+		double a = scr.nextDouble();
+		double b = scr.nextDouble();
+		
+		System.out.println(a/b);
+	}
+}
---
+++ stage_2/1008.py
@@ -0,0 +1,9 @@
+/*
+ * 백준 온라인 저지 1008번(A/B) 문제풀이
+ *
+ * @see https://www.acmicpc.net/problem/1008
+ * @Created by Jeonghun on 2017-08-22
+ **/
+
+a,b= map(float,input().split())
+print(a/b)
